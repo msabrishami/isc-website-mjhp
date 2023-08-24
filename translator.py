6ebc7fd2a1227f8590233aaeafe68af16521f632
msabrishami <abri442@usc.edu>
2023-08-24 12:33:24 -0700

    translator works fine foe sample markdown

--- translator.py
+++ translator.py
@@ -1,7 +1,88 @@
 import re
 import config
+import pdb
 
 html = config.top_html
+
+def gen_paragraph(par, style):
+    if style in ["h1", "h2"]:
+        res = f'''
+            <p>{par}</p>
+            '''
+
+    elif style == "h3":
+        res = f'''
+            <p class="ms-3">{par}</p>
+            '''
+
+    return res
+
+def md2html_pars(pars, style):
+    res = ""
+    in_list = False
+    in_note = False
+    if isinstance(pars, str):
+        pars = ["".join(pars)]
+    for line in pars:
+        print(line)
+		# bold and italic 
+        line = re.sub(r'\*\*(.*?)\*\*', r'<strong>\1</strong>', line)
+        line = re.sub(r'\*(.*?)\*', r'<i>\1</i>', line)
+
+        # Note boxes
+        if line.strip() == "```":
+            if in_note:
+                res += '</div>\n'
+                in_note = False
+            else:
+                if in_list:     # just ad hoc modification
+                    res += '</ul>\n'
+                    in_list = False
+                res += '<div class="alert alert-primary" role="alert">\n'
+                in_note = True
+            continue
+
+
+        # itemized list, the line starts with '*'
+        if line.startswith('*'):
+            if not in_list:
+                res += '<ul>\n'
+                in_list = True
+            res  += f'<li>{line[1:].strip()}</li>\n'
+        else:
+            if in_list:
+                res += '</ul>\n'
+                in_list = False
+			
+			# based on style paragraph can be different
+            if in_note:
+                res+= line
+            elif style in ["h1", "h2"]:
+                res += f'<p>{line}</p>\n'
+            elif style in ["h3"]:
+                res += f'<p class="ms-3">{line}</p>\n'
+            else:
+                print("Warning: paragraph with unknown style!")
+                res += f'<p>{line}</p>\n'
+                
+    if in_list:
+        res += '</ul>'
+    if in_note: 
+        res += '</div>\n'  
+    
+    return res
+
+def md2html_notes(notes):
+    res = ""
+    for note in notes:
+        res += '''
+                <!-- ======= Note ======= -->
+                <div class="alert alert-primary" role="alert">
+                    {note}
+                </div>
+        '''
+    return res 
+
 
 # Sample input
 with open("input.md", "r") as file:
@@ -13,35 +94,62 @@ h2s = []
 current_h2 = None
 current_h3 = None
 intro_text = []
+current_level = None
 
 # Process lines
 for line in lines:
     line = line.strip()
+    # if not line:
+    #     continue
+    print(f".{len(line)}" + line)
+    
+    # if "alef" in line:
+    #     pdb.set_trace()
 
     if line.startswith("# "):
         h1 = line[2:]
+
     elif line.startswith("## "):
         if current_h2:
+            if current_h3:
+                current_h2['h3s'].append(current_h3)
+                current_h3 = None
             h2s.append(current_h2)
+        
         current_h2 = {'title': line[3:], 'h3s': [], 'paragraphs': []}
+        current_level = 'h2'
+
     elif line.startswith("### "):
         if current_h3:
             current_h2['h3s'].append(current_h3)
-        current_h3 = {'title': line[4:], 'paragraph': None}
-    elif line.startswith("```"):
-        pass  # If notes are needed for HTML generation, process here
-    elif line and current_h2:
-        if current_h3:
-            current_h3['paragraph'] = line
-            current_h2['h3s'].append(current_h3)
-            current_h3 = None
-        else:
+        current_h3 = {'title': line[4:], 'paragraphs': []}
+        current_level = 'h3'
+    # elif line.startswith("```"):
+    #     if in_note_box:
+    #         in_note_box = False
+    #         current_h2['notes'].append(' '.join(note_content))
+    #         note_content = []
+    #     else:
+    #         in_note_box = True
+
+    # elif line and in_note_box:
+    #     note_content.append(line)
+    elif line:
+        if current_level == 'h3':
+            current_h3['paragraphs'].append(line)
+        elif current_level == 'h2':
             current_h2['paragraphs'].append(line)
-    elif line and not current_h2:  # Introduction text after H1 and before first H2
-        intro_text.append(line)
+        else: # this is the intro for h1 right before any h2
+            intro_text.append(line)
 
 if current_h2:
+    if current_h3:
+        current_h2['h3s'].append(current_h3)
     h2s.append(current_h2)
+
+
+pdb.set_trace()
+
 
 # Generate HTML
 html += f'''
@@ -66,9 +174,6 @@ icons = [
     "mdi-credit-card", "mdi-checkbook"
 ]
 
-
-import pdb
-pdb.set_trace()
 for idx, h2 in enumerate(h2s):
     secid = re.sub(r'[^a-z0-9]', '', h2['title'].lower())
     html += f'''
@@ -84,6 +189,7 @@ html += '''
 '''
 
 for paragraph in intro_text:
+    print("." + paragraph)
     html += f'''
                 <p>{paragraph}</p>
     '''
@@ -94,6 +200,7 @@ html += '''
 
 for h2 in h2s:
     secid = re.sub(r'[^a-z0-9]', '', h2['title'].lower())
+    # creating the h2 linked to the outline
     html += f'''
                 <section id="secid{secid}">
 					<div class="title-lvl2">
@@ -103,15 +210,22 @@ for h2 in h2s:
 						</div>
 					</div>	
     '''
-    for paragraph in h2['paragraphs']:
-        html += f'''
-                    <p>{paragraph}</p>
-        '''
+    
+    # creating the intro below h2 
+    print(h2['title'])
+    html += md2html_pars(h2['paragraphs'], "h2")
+    # html += md2html_notes(h2['notes'])
+    # for paragraph in h2['paragraphs']:
+    #     print("." + paragraph)
+    #     html += gen_paragraph(paragraph, 'h2')
+
+    # creating the h3s within this h2
     for h3 in h2['h3s']:
         html += f'''
                     <h5>{h3['title']}</h5>
-                    <p class="ms-3">{h3['paragraph']}</p>
         '''
+        # html += gen_paragraph(h3['paragraph'], 'h3')
+        html += md2html_pars(h3['paragraphs'], 'h3')
 
     html += '''
                 </section>
